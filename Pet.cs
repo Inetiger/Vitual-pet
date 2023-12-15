7a3817b226a6304c8502d03ddbca473c992349fa
Inetiger <ivensen18@gmail.com>
2023-12-15 12:24:56 +0100

    Add project files.

---
+++ Pet.cs
@@ -0,0 +1,16 @@
+﻿namespace VirtualPet
+{
+    internal class Pet
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Species { get; private set; }
+
+        public Pet(string name, int age, string species)
+        {
+            Name = name;
+            Age = age;
+            Species = species;
+        }
+    }
+}
